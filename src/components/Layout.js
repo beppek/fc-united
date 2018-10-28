0ec437f4df44d474d33c3e6d354ef4d6d9343985
Beppe <beppe@live.se>
2018-10-28 23:35:30 +1100

    Add favicon

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -6,7 +6,13 @@ import './all.sass';
 
 const TemplateWrapper = ({ children }) => (
   <div>
-    <Helmet title="FC United  | Probably the best floorball team in Victoria" />
+    <Helmet title="FC United  | Probably the best floorball team in Victoria">
+      <meta
+        name="description"
+        content="FC United, Probably the best floorball team in Victoria"
+      />
+      <link rel="icon" type="image/png" href="/favicon.png" />
+    </Helmet>
     <Navbar />
     <div>{children}</div>
   </div>
