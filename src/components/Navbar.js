f1331672643e63f447b8515caffa081cda8d3dec
Beppe Karlsson <beppe@live.se>
2018-11-12 12:43:55 +1100

    Use real logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'gatsby';
-import logo from '../img/floorball.svg';
+import logo from '../img/united-logo.svg';
 
 const Navbar = () => (
   <nav className="navbar is-transparent">
