db9b46de1afa2817dd6b41ff11ac06239bb2dd35
Beppe Karlsson <beppe@live.se>
2018-11-12 12:44:09 +1100

    Add latest trophy (eagles)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import Layout from '../components/Layout';
-import latestKill from '../img/dead-penguin.png';
+import deadPenguin from '../img/dead-penguin.png';
+import deadEagle from '../img/dead-eagle.png';
+import logo from '../img/united-logo.svg';
 
 const IndexPage = () => {
   return (
@@ -8,14 +10,11 @@ const IndexPage = () => {
       <section className="section">
         <div className="container">
           <div className="content">
-            <h1 className="has-text-weight-bold is-size-2">FC United</h1>
+            <h1 className="has-text-weight-bold is-size-2">United FC</h1>
             <h3>Probably the best floorball team in Victoria</h3>
-            <img
-              src={latestKill}
-              alt="dead penguin"
-              title="Our latest victim"
-            />
-            <p>Our latest victim</p>
+            <img src={deadPenguin} alt="dead penguin" title="Victim number 1" />
+            <img src={deadEagle} alt="dead eagle" title="Victim number 2" />
+            <h4>Our trophies</h4>
           </div>
         </div>
       </section>
